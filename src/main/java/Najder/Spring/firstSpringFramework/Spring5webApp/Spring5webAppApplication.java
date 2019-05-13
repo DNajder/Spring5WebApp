ddbc328f01f04d922e0cd5a7ceb15c7a2e8d457e
Darek Najder <losman1987naj@gmail.com>
2019-05-13 17:36:43 +0200

    first commit

---
+++ src/main/java/Najder/Spring/firstSpringFramework/Spring5webApp/Spring5webAppApplication.java
@@ -0,0 +1,13 @@
+package Najder.Spring.firstSpringFramework.Spring5webApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webAppApplication.class, args);
+	}
+
+}
