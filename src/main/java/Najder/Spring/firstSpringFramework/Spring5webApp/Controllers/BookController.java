891200dadf77aee29a32f67b58e08fd9df6df9a4
Darek Najder <losman1987naj@gmail.com>
2019-05-29 14:29:45 +0200

    add first controller

---
+++ src/main/java/Najder/Spring/firstSpringFramework/Spring5webApp/Controllers/BookController.java
@@ -0,0 +1,23 @@
+package Najder.Spring.firstSpringFramework.Spring5webApp.Controllers;
+
+import Najder.Spring.firstSpringFramework.Spring5webApp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "books";
+    }
+
+}
